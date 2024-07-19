567ee2e3421e73a18075855c96553dbc3e71cd69
TENSHKUMAR K <tenshkumarkkt@gmail.com>
2024-07-19 14:55:36 +0530

    DB models created for Users and Properties

---
+++ server/db/models/propertiesSchema.js
@@ -0,0 +1,35 @@
+const mongoose = rquire('mongoose')
+
+const PropertySchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    propertyType: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    photo: {
+        type: String,
+        required: true
+    },
+    creater: {
+        type: mongoose.Schema.Types.ObjectId, ref: 'User'
+    },
+})
+
+const propertyModel = mongoose.model('Property',PropertySchema)
+
+module.exports = {propertyModel}
---
+++ server/db/models/userSchema.js
@@ -0,0 +1,23 @@
+const mongoose = rquire('mongoose')
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    avatar: {
+        type: String,
+        required: true
+    },
+    allProperties: [{
+        type: mongoose.Schema.Types.ObjectId, ref: 'Property'
+    }]
+})
+
+const userModel = mongoose.model('User',UserSchema)
+
+module.exports = {userModel}
